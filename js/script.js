8c14f9ef50f53809963753eee32020d3186a790b
fescolp-tech <ifeoluwaolatunji583@gmail.com>
2025-10-06 22:16:06 +0100

    new

--- js/script.js
+++ js/script.js
@@ -27,6 +27,34 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ////////////////////////////////////////////////////////
+// SMOOTH SCROLLING AMINATION
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement("div");
